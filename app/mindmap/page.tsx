fdf126757d3f7ce73064805f3852fb8817cb3592
Forge (Dev Builder) <jneilloutside@gmail.com>
2026-06-08 15:54:27 -0700

    fix: force-dynamic mindmap page + latest SVG with Waterloo node

--- app/mindmap/page.tsx
+++ app/mindmap/page.tsx
@@ -1,6 +1,9 @@
 import fs from 'fs'
 import path from 'path'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export default function MindmapPage() {
   const html = fs.readFileSync(path.join(process.cwd(), 'public/project-mindmap.html'), 'utf8')
   return <div dangerouslySetInnerHTML={{ __html: html }} />
